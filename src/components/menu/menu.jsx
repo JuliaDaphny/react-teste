df928b504b4fff63573ce4c756f97b400576ccd2
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-03-22 21:28:54 -0300

    Add files via upload

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const menu = () => {
+  return (
+    <div>
+        <ul>
+            <li><a href="">bla1</a></li>
+            <li><a href="">bla2</a></li>
+            <li><a href="">bla3</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default menu
