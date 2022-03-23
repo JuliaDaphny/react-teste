4e503da903c7b37c41e7a05e5a346641b0ddea12
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-03-22 22:05:08 -0300

    commit de alteração

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import carai from './pages/footer.svg';
+import Menu from './components/menu/menu';
+import {Button, Alert} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={carai} className="A" alt="carai" />
         <p>
           girando infinito no passinho do
         </p>
@@ -17,7 +22,12 @@ function App() {
         >
           hello world
         </a>
+        <Button variant='outline-secondary'>bla bla bla</Button>
+        <Alert>bla bla bla</Alert>
       </header>
+      
+     
+      
     </div>
   );
 }
