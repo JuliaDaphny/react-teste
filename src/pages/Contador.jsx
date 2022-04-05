b132a498c3cf874025673e29f3268cfd64771a28
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-04-05 20:58:44 -0300

    up requisitos

--- src/pages/Contador.jsx
+++ src/pages/Contador.jsx
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
 import { Button } from 'react-bootstrap'
 const Contador = () => {
+    //declaração state numero
     let [numero, setNumero] = useState()
     
   return (
     <div>
-     <Button variant='sucess' onClick={setNumero(++numero)}/>
+     <Button variant='sucess' onClick={setNumero(numero= numero +10)}>+10</Button>
+     <Button variant='sucess' onClick={setNumero(numero = numero +5)}>+5</Button>
+     <Button variant='sucess' onClick={setNumero(++numero)}>+</Button>
      {numero}
-     <Button variant='danger' onClick={setNumero(--numero)}/>
+     <Button variant='danger' onClick={setNumero(--numero)}>-</Button>
+     <Button variant='danger' onClick={setNumero(numero = numero -  5)}>-5</Button>
+     <Button variant='danger' onClick={setNumero(numero = numero - 10)}>-10</Button>
     </div>
   )
 }
