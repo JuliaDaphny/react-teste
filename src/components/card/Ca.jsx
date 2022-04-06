509aa791abe6ae259c0d05934050e9a1ebd4cf5c
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-04-05 22:47:35 -0300

    atualização card

--- src/components/card/Ca.jsx
+++ src/components/card/Ca.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap';
+
 const Ca = (x) => {
   return (
     <div>
       <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={x.imagem} />
+  <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500'+x.imagem} />
   <Card.Body>
     <Card.Title>{x.marca} - {x.modelo}</Card.Title>
     <Card.Text>
-      <strong>Cor : </strong>{x.cor}
+      <strong>idioma : </strong>{x.cor}
     </Card.Text>
     <Card.Text>
       <strong>Ano : </strong>{x.ano}
--- src/pages/filmes/Filmes.jsx
+++ src/pages/filmes/Filmes.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React,{useState, useEffect} from 'react'
+import tmdb from '../../services/tmdb'
+import{Row , Col} from "react-bootstrap"
+import Ca from "../../components/card/Ca"
 const Filmes = () => {
+  // declaração de state 
+  
+  let [filmes,setFilmes] = useState([])
+  useEffect(()=>{
+    async function data() {
+      const pega = await tmdb.get('/list/1');
+      const data= pega.data.results
+      setFilmes(data)
+    }
+    data();
+  },[])
   return (
     <div>
-        bla bla bla
+        <Row xs={1} md={4} >
+      
+      {filmes.map(item=>(
+         <Col>   
+        <Ca  marca={item.title} modelo={item.adult} imagem={item.poster_path} cor={item.original_language} ano={item.release_date} nomebotao='Mais detalhes' />
+        </Col>
+      ))}
+      
+      </Row>
         
     </div>
   )
